fcfb5d453fba61b23101ff9940822851a321ca5a
NicuVlasin <vlasin_nicu@yahoo.com>
2017-12-09 17:49:26 +0000

    Spring Security + Authentication update

---
+++ Front-end/src/main/java/sdh4/adf/grp2/entities/SpringBootWebApplication.java
@@ -0,0 +1,10 @@
+package sdh4.adf.grp2.entities;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
